29e3666579742e08160be08e2a60b4ffeaac4361
patrickZWY <154947644+patrickZWY@users.noreply.github.com>
2025-05-08 19:19:33 -0400

    start anew

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './NavBar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Profile from './pages/Profile';
+import { Route, Routes } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Navbar />
+      <div className='container'>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/about" element={<About />}/>
+          <Route path="/register" element={<Register />}/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/profile" element={<Profile />}/>
+        </Routes>
+      </div>
+    </>
+  )
 }
 
 export default App;
---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+export default function About() {
+    return <h1>                
+        <p>
+            Welcome to Archipelago!
+        </p>
+        <p>
+            Use the navigation menu to explore movies, view your profile, or manage connections.
+        </p>
+        </h1>
+}
